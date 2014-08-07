55ea3a0ad3f17c80d2b994d4d7e85b14a3bc0777
Eran <me@eran.sh>
2014-08-07 13:25:17 -0400

    This is why you don't write HTML in such a ridiculous manner

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ var requestHandler = function(req, res){
 	if( Math.abs(new Date() - lastRead) >= 300000 ) redditGetter();
 
 	res.writeHead(200);
-	res.write('<html><head><meta charset="utf-8" /></head>');
+	res.write('<html><head><meta charset="utf-8" />');
 	res.write('<style type="text/css">html,body{width:100vw;height:100vh;margin:0;padding:0}h1,footer{font-family:helvetica, arial;color: rgb(241, 241, 241);max-width: 350px;;margin:1em auto;padding:0}p{font-family:helvetica, arial;display:block;max-width: 350px;;margin:0 auto;padding:0}p {opacity:0;transition-duration:3s}p.loaded {opacity:1}.blast-root .blast:nth-of-type(2n) {color: rgb(233, 109, 109);}.blast {color: rgb(255, 255, 255);transition-duration:10s}.loaded .blast {color: rgb(201, 201, 201);}footer{text-align:center;}footer a{color: rgb(141, 141, 141);text-decoration: none;}.slabtexted .slabtext{display:-moz-inline-box;display:inline-block;white-space:nowrap;}.slabtextinactive .slabtext{display:inline;white-space:normal;font-size:1em !important;letter-spacing:inherit !important;word-spacing:inherit !important;*letter-spacing:0 !important;*word-spacing:0 !important;}.slabtextdone .slabtext{display:block;}</style>');
 	res.write('</head><body>');
 	res.write('<h1>reddit headlines</h1>');
